52feebdd1cb98fe7a936e17e9710842139f252ac
kasemkamel <kss251477@gmail.com>
2023-03-30 23:13:29 +0200

    imp

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,11 @@
 
 import 'package:flower_app/pages/checkout.dart';
 import 'package:flower_app/pages/details_screen.dart';
+import 'package:flower_app/pages/home.dart';
+import 'package:flower_app/pages/login.dart';
+import 'package:flower_app/pages/register.dart';
+import 'package:flower_app/provider/cart.dart';
+import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
 
