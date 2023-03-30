3f07cee04843e4a94ed51b0b4e10e566e7816a47
kasemkamel <kss251477@gmail.com>
2023-03-30 22:15:32 +0200

    dou

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -29,6 +29,24 @@ class Login extends StatelessWidget {
                     // fillColor: Colors.red,
                     filled: true,
                     contentPadding: const EdgeInsets.all(8),
+                  )),
+              TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Your Email : ",
+                    // To delete borders
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: Divider.createBorderSide(context),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    // fillColor: Colors.red,
+                    filled: true,
+                    contentPadding: const EdgeInsets.all(8),
                   ))
             ],
           ),
