b34fe3a28a9fa5b09517102cb9ec4d7ae830ff9f
kasemkamel <kss251477@gmail.com>
2023-03-30 23:10:49 +0200

    positioned

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,39 @@
-import 'package:flutter/material.dart';
-import 'package:kasem_app/pages/login.dart';
 
+
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flower_app/pages/checkout.dart';
+import 'package:flower_app/pages/details_screen.dart';
+import 'package:flower_app/pages/home.dart';
+import 'package:flower_app/pages/login.dart';
+import 'package:flower_app/pages/register.dart';
+import 'package:flower_app/provider/cart.dart';
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
 void main() {
   runApp(const MyApp());
 }
-
+ 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+ 
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Login(),
+    return     ChangeNotifierProvider(
+      create: (context) {return Cart();},
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+      ),
     );
   }
 }
+
+
+
+
+
+
+
+
---
+++ lib/shared/appbar.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flower_app/pages/checkout.dart';
+import 'package:flower_app/provider/cart.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ProductsAndPrice extends StatelessWidget {
+  const ProductsAndPrice({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final carttt = Provider.of<Cart>(context);
+    return Row(
+      children: [
+        Stack(
+          children: [
+            Positioned(
+              bottom: 24,
+              child: Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(211, 164, 255, 193),
+                      shape: BoxShape.circle),
+                  child: Text(
+                    "${carttt.itemCount}",
+                    style: TextStyle(
+                        fontSize: 16, color: Color.fromARGB(255, 0, 0, 0)),
+                  )),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
